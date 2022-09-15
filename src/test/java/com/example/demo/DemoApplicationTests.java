6e401bc2429a3c668f78d9cf8bda7b6d2b6677f2
t-araseki <t-araseki@nec.com>
2022-09-15 13:12:27 +0900

    update test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class DemoApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    public void testHello() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port, String.class), is("Hello World!"));
+    }
 
 }
